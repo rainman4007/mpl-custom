5d4f1b800beaf854dbbc7a6fdca82ab1b8647dc9
Rain Man <triv.gattu@gmail.com>
2021-03-23 21:10:54 -0400

    testing custom module

--- resources/com/triv/devops/mpl/modules/Echo/TestEcho.groovy
+++ resources/com/triv/devops/mpl/modules/Echo/TestEcho.groovy
@@ -2,6 +2,6 @@ echo "testing sub module"
 print("global var from Test Echo "+ globalVars.name + " " + globalVars.nameMaps)
 
 globalVars.name = "newValue"
-globalVars.nameMaps['name'] = "Test"
+globalVars.nameMaps = ['name': "Test"]
 
 OUT.'artifact.version' = 1
