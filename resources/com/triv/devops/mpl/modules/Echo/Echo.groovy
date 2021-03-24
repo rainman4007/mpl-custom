a184bb49a94e62d05a498bbd73abe10be709bf7a
Rain Man <triv.gattu@gmail.com>
2021-03-23 20:54:16 -0400

    testing custom module

--- resources/com/triv/devops/mpl/modules/Echo/Echo.groovy
+++ resources/com/triv/devops/mpl/modules/Echo/Echo.groovy
@@ -1,5 +1,5 @@
 def version = MPLModule('Test Echo', CFG).'artifact.version'
 echo "${version}"
 OUT.'artifact_info.artifact.version' = version
-MPLPipelineConfigMerge(MPLModule('Test Echo', CFG).artifact_info)
+//MPLPipelineConfigMerge(MPLModule('Test Echo', CFG).artifact_info)
 echo "${CFG}"
