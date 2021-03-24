33ff7548fc7bee091b8f7d4dbdfde2f45da0bf2f
Rain Man <triv.gattu@gmail.com>
2021-03-24 18:39:15 -0400

    using dynamic pipeline

---
+++ vars/DynamicPipeline.groovy
@@ -0,0 +1,49 @@
+def call(body) {
+  library('mpl')
+  MPLModulesPath('com/triv/devops/mpl')
+
+  def MPL = MPLPipelineConfig(body, [
+    agent_label: '',
+    modules: [:]
+  ])
+  
+  
+  pipeline {
+    agent {
+      label MPL.agentLabel
+    }
+
+    stages {
+      stage('Initialize') {
+        steps {
+          script {
+            
+            // For each key in MPL.modules produce a stage with that name
+            MPL.modules.each { module ->
+              print("Initializing with stage ${module}.")
+              if ( MPLModuleEnabled(module) ) {
+                stage(module) {
+                  MPLModule()
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    
+    post {
+      always {
+        MPLPostStepsRun('always')
+      }
+
+      success {
+        MPLPostStepsRun('success')
+      }
+
+      failure {
+        MPLPostStepsRun('failure')
+      }
+    }
+  }
+}
