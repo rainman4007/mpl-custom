3fe99204920b64534c493300f2f9dbb2627cabb8
Rain Man <triv.gattu@gmail.com>
2021-03-24 21:02:24 -0400

    testing

--- vars/MPLPipelineConfig.groovy
+++ vars/MPLPipelineConfig.groovy
@@ -29,19 +29,35 @@ import com.griddynamics.devops.mpl.MPLException
  * MPL pipeline helper to provide the default modules configuration
  *
  * @author Sergei Parshev <sparshev@griddynamics.com>
+ *
  * @param body      Configuration for the pipeline
  * @param defaults  Default configuration from the pipeline definition
  * @param overrides Mandatory settings for the pipeline that will override config settings
+ *
  * @return MPLManager singleton object
  */
 def call(body, Map defaults = [:], Map overrides = [:]) {
   def config = defaults
+  MPLManager mplManager = MPLManager.instance
 
   // Merging configs
   if( body in Closure ) {
+    // This logic allow us to use configuration closures instead of maps
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
+
+    // Make sure the global variables will be available in the config closure
+    config.env = env
+    config.params = params
+    config.currentBuild = currentBuild
+
+    // Here we executing the closure to update the pipeline defaults with the closure values
     body()
+
+    // Removing the global variables from the config
+    config.remove('env')
+    config.remove('params')
+    config.remove('currentBuild')
   } else if( body in Map ) {
     Helper.mergeMaps(config, body)
   } else
@@ -50,5 +66,5 @@ def call(body, Map defaults = [:], Map overrides = [:]) {
   Helper.mergeMaps(config, overrides)
 
   // Init the MPL Pipeline
-  MPLManager.instance.init(config)
+  mplManager.init(config)
 }
