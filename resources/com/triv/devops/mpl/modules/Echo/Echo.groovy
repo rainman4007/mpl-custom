a26ba58ae9fe6b065b32921ba30b87b9f97965ff
Rain Man <triv.gattu@gmail.com>
2021-03-31 20:53:01 -0400

    testing

--- resources/com/triv/devops/mpl/modules/Echo/Echo.groovy
+++ resources/com/triv/devops/mpl/modules/Echo/Echo.groovy
@@ -3,8 +3,9 @@ MPLPostStep('always') {
 }
 
 print("CFG from Echo "+ CFG)
-def version = MPLModule('Test Echo', CFG).'artifact.version'
-echo "${version}"
-OUT.'artifact_info.artifact.version' = version
+//def version = MPLModule('Test Echo', CFG).'artifact.version'
+MPLModule('Test Echo', CFG)
+//echo "${version}"
+//OUT.'artifact_info.artifact.version' = version
 //MPLPipelineConfigMerge(MPLModule('Test Echo', CFG).artifact_info)
 print("global var from Echo "+ globalVars.name)
